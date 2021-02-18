74d240f2cabf0792eec4ba572bb511ae26e78ffe
Evans mondragon <evananyta@hotmail.com>
2021-02-17 23:01:44 -0500

    Responsive design

--- js/app.js
+++
@@ -1,8 +0,0 @@
-// User options
-new Accordion('.accordion-container', {
-    duration: 400,
-    showMultiple: true,
-    onOpen: function(currentElement) {
-      console.log(currentElement);
-    }
-  });
